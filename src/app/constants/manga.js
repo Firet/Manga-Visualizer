d39af7cd1aedc8fe98ec16f2ac04138526e83b87
Firet <damianfederico@protonmail.com>
2024-09-09 18:14:18 -0300

    Create manga constant and render them all with .map() in MangaCard

---
+++ src/app/constants/manga.js
@@ -0,0 +1,54 @@
+
+const mangas = [
+	{
+		id: 1,
+		name: 'Bleach',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+    {
+		id: 2,
+		name: 'Bleach',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+	{
+		id: 3,
+		name: 'Bleach',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+	{
+		id: 4,
+		name: 'Bleach',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+	{
+		id: 5,
+		name: 'Bleach',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+	{
+		id: 6,
+		name: 'Chainsaw man',
+        alt: 'Tapa del manga',
+		description:
+			'Bleach trata sobre cazar dragones',
+		cover: './bleach-74.png',
+	},
+
+];
+
+export default mangas;
--- src/components/MangaCard/MangaCard.js
+++ src/components/MangaCard/MangaCard.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Image from "next/image";
+import mangas from '@/app/constants/manga';
 
 export default function MangaCard() {
+    mangas;
 
     return (
         <div className="">
-            <p>Bleach</p>
-            <Image
-                src="./bleach-74.png"
-                alt="Tapa de Bleach"
-                className="manga-cover"
-                unoptimized
-                width={176}
-                height={224}
-            />
+            <ul >
+                {mangas.map(manga => (
+                    <li key={manga.id}>
+                        <p>{manga.name}</p>
+                        <Image
+                            src={manga.cover}
+                            alt={manga.alt}
+                            className="manga-cover"
+                            unoptimized
+                            width={176}
+                            height={224}
+                        />
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
