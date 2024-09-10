39f4dcc350cb9c0cf3dddd66e7d90ca1c9b5c1ad
Firet <damianfederico@protonmail.com>
2024-09-10 08:11:55 -0300

    Pass mangaId when iterate in Manga Collection component

--- src/components/MangaCollection/MangaCollection.js
+++ src/components/MangaCollection/MangaCollection.js
@@ -8,7 +8,7 @@ export default function MangaCollection() {
 	return (
 		<div className="manga-collection">
             {mangas.map((manga) => (
-                    <MangaCard key={manga.id} name={manga.name} src={manga.cover} alt={manga.alt}/>
+                    <MangaCard key={manga.id} mangaId={manga.id} name={manga.name} src={manga.cover} alt={manga.alt}/>
                 ))}
 		</div>
 	);
