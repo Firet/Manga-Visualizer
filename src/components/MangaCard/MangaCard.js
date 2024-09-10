3067b1624664cd2be9cf785432a058111116700a
Firet <damianfederico@protonmail.com>
2024-09-10 08:10:13 -0300

    Add manga card link to manga detail using next router

--- src/components/MangaCard/MangaCard.js
+++ src/components/MangaCard/MangaCard.js
@@ -1,20 +1,24 @@
+'use client'
+
 import React from 'react';
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
 import './MangaCard.css'
 
-export default function MangaCard({ name, src, alt }) {
+export default function MangaCard({ mangaId, name, src, alt }) {
+    const router = useRouter();
 
     return (
-        <div className="manga-card-container">
-                <p className='manga-main-title'>{name}</p>
-                <Image
-                    src={src}
-                    alt={alt}
-                    className="manga-cover"
-                    unoptimized
-                    width={176}
-                    height={224}
-                />
+        <div className="manga-card-container" onClick={() => router.push(`/mangas/${mangaId}`)}>
+            <p className='manga-main-title'>{name}</p>
+            <Image
+                src={src}
+                alt={alt}
+                className="manga-cover"
+                unoptimized
+                width={176}
+                height={224}
+            />
         </div>
     );
 }
