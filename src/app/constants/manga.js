cb4ef947f54ad06459c7ee46a2cad8df1318a87c
Firet <damianfederico@protonmail.com>
2024-09-10 04:14:26 -0300

    Add manga covers, titles and descriptions to manga.js

--- src/app/constants/manga.js
+++ src/app/constants/manga.js
@@ -5,50 +5,84 @@ const mangas = [
 		name: 'Bleach',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
+			'Bleach trata sobre cazar demonios pero no estoy seguro',
 		cover: './bleach-74.png',
 	},
     {
 		id: 2,
-		name: 'Bleach',
+		name: 'Macross',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
-		cover: './bleach-74.png',
+			'Mientras lo leés rememorás la canción del opening',
+		cover: 'https://meo2.comick.pictures/m3KMk.jpg',
 	},
 	{
 		id: 3,
-		name: 'Bleach',
+		name: 'Evangelion',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
-		cover: './bleach-74.png',
+			'Mommy issues mientras leés la biblia.',
+		cover: 'https://meo2.comick.pictures/w7pJYM.jpg',
 	},
 	{
 		id: 4,
-		name: 'Bleach',
+		name: 'Akira',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
-		cover: './bleach-74.png',
+			'Andar en moto mientras tiran una bomba nuclear.',
+		cover: 'https://meo2.comick.pictures/Z806Bj.png',
 	},
 	{
 		id: 5,
-		name: 'Bleach',
+		name: 'Dorohedoro',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
-		cover: './bleach-74.png',
+			'Oscuridad y dibujos inconsistentes. El mejor manga que hay.',
+		cover: 'https://meo2.comick.pictures/q5R31.jpg',
 	},
 	{
 		id: 6,
 		name: 'Chainsaw man',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar dragones',
-		cover: './bleach-74.png',
+			'El chico motosierra, el animé de moda hoy en día.',
+		cover: 'https://meo2.comick.pictures/Og84N.png',
 	},
-
+	{
+		id: 7,
+		name: 'Capitain Tsubasa',
+		alt: 'Tapa del manga',
+		description: 'Mi sueño siempre fue ganar el mundial',
+		cover: 'https://meo2.comick.pictures/3en7Q-s.jpg',
+	},
+	{
+		id: 8,
+		name: 'Ranma 1/2',
+		alt: 'Tapa del manga',
+		description: 'No sé si el pelo rojo le queda mejor',
+		cover: 'https://meo2.comick.pictures/w7pYNR.png',
+	},
+	// {
+	// 	id: 8,
+	// 	name: 'Ranma 1/2',
+	// 	alt: 'Tapa del manga',
+	// 	description: 'No sé si el pelo rojo le queda mejor',
+	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
+	// },
+	// {
+	// 	id: 8,
+	// 	name: 'Ranma 1/2',
+	// 	alt: 'Tapa del manga',
+	// 	description: 'No sé si el pelo rojo le queda mejor',
+	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
+	// },
+	// {
+	// 	id: 8,
+	// 	name: 'Ranma 1/2',
+	// 	alt: 'Tapa del manga',
+	// 	description: 'No sé si el pelo rojo le queda mejor',
+	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
+	// },
 ];
 
 export default mangas;
