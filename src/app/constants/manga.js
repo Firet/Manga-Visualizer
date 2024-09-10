a7576f15f708f47b486f7a9481f0a20449ebefc5
Firet <damianfederico@protonmail.com>
2024-09-10 11:22:53 -0300

    Add more manga info to manga.js

--- src/app/constants/manga.js
+++ src/app/constants/manga.js
@@ -5,7 +5,7 @@ const mangas = [
 		name: 'Bleach',
         alt: 'Tapa del manga',
 		description:
-			'Bleach trata sobre cazar demonios pero no estoy seguro',
+			'Bleach trata sobre cazar demonios pero no estoy seguro.',
 		cover: './bleach-74.png',
 	},
     {
@@ -13,7 +13,7 @@ const mangas = [
 		name: 'Macross',
         alt: 'Tapa del manga',
 		description:
-			'Mientras lo leés rememorás la canción del opening',
+			'Mientras lo leés rememorás la canción del opening.',
 		cover: 'https://meo2.comick.pictures/m3KMk.jpg',
 	},
 	{
@@ -52,37 +52,58 @@ const mangas = [
 		id: 7,
 		name: 'Captain Tsubasa',
 		alt: 'Tapa del manga',
-		description: 'Mi sueño siempre fue ganar el mundial',
+		description: 'Mi sueño siempre fue ganar el mundial.',
 		cover: 'https://meo2.comick.pictures/3en7Q-s.jpg',
 	},
 	{
 		id: 8,
 		name: 'Ranma 1/2',
 		alt: 'Tapa del manga',
-		description: 'No sé si el pelo rojo le queda mejor',
+		description: 'No sé si el pelo rojo le queda mejor.',
 		cover: 'https://meo2.comick.pictures/w7pYNR.png',
 	},
-	// {
-	// 	id: 8,
-	// 	name: 'Ranma 1/2',
-	// 	alt: 'Tapa del manga',
-	// 	description: 'No sé si el pelo rojo le queda mejor',
-	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
-	// },
-	// {
-	// 	id: 8,
-	// 	name: 'Ranma 1/2',
-	// 	alt: 'Tapa del manga',
-	// 	description: 'No sé si el pelo rojo le queda mejor',
-	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
-	// },
-	// {
-	// 	id: 8,
-	// 	name: 'Ranma 1/2',
-	// 	alt: 'Tapa del manga',
-	// 	description: 'No sé si el pelo rojo le queda mejor',
-	// 	cover: 'https://meo2.comick.pictures/w7pYNR.png',
-	// },
+	{
+		id: 9,
+		name: 'Dragon Ball',
+		alt: 'Tapa del manga',
+		description: 'Clásico de clásicos.',
+		cover: 'https://meo2.comick.pictures/o1G7k.jpg',
+	},
+	{
+		id: 10,
+		name: 'Saint Seiya',
+		alt: 'Tapa del manga',
+		description: 'Los guardianes del universo al triunfar el mal.',
+		cover: 'https://meo2.comick.pictures/EOmEyx.jpg',
+	},
+	{
+		id: 11,
+		name: 'Slam Dunk',
+		alt: 'Tapa del manga',
+		description: 'El mejor manga de deportes.',
+		cover: 'https://meo2.comick.pictures/R75wq.jpg',
+	},
+	{
+		id: 12,
+		name: 'Detective Conan',
+		alt: 'Tapa del manga',
+		description: 'De la época del club del anime.',
+		cover: 'https://meo2.comick.pictures/2krvL.jpg',
+	},
+	{
+		id: 13,
+		name: 'Beta X',
+		alt: 'Tapa del manga',
+		description: 'Calculadoras, computadoras cada invento nos acerca cada vez más a la destrucción.',
+		cover: 'https://meo2.comick.pictures/XN7NJ.jpg',
+	},
+	{
+		id: 14,
+		name: 'La visión de Escaflowne',
+		alt: 'Tapa del manga',
+		description: 'Uniformes verdes y magia, supongo.',
+		cover: 'https://meo2.comick.pictures/ORlYb.jpg',
+	},
 ];
 
 export default mangas;
