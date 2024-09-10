d2d769d411c69586a737b935025f74cab65dd975
Firet <damianfederico@protonmail.com>
2024-09-10 03:51:43 -0300

    Use only one button to change theme in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ import './Header.css'
 
 export default function Header() {
     const { theme, setTheme } = useTheme();
-        return (
+    return (
         <header>
             <h1 className="header-title">Manga Display</h1>
-            The current theme is: {theme}
-            <button onClick={() => setTheme('light')}>Light Mode</button>
-            <button onClick={() => setTheme('dark')}>Dark Mode</button>
+            <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+                {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+            </button>
         </header>
     );
 }
