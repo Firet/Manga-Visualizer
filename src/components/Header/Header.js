dcf8e7bb8350845d5319d913dd25c42b3fa95eee
Firet <damianfederico@protonmail.com>
2024-10-08 18:56:49 -0300

    Fix 'text content doesn't match server render html' bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import './Header.css';
 
 export default function Header() {
 	const { theme, setTheme } = useTheme();
-	return (
-		<header>
-			<h1 className="header-title">Manga Display</h1>
-			<button
-				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-			>
-				{theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-			</button>
-		</header>
-	);
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setIsClient(true)
+	}
+		, [])
+
+	if (isClient) {
+		return (
+			<header>
+				<h1 className="header-title">Manga Display</h1>
+				<button
+					onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+				>
+					{theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+				</button>
+			</header>
+		);
+	}
 }
