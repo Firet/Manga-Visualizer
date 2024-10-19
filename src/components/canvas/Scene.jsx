fd747f6c0d90e0f7df02ccba3dc5289aee8ab839
Firet <damianfederico@protonmail.com>
2024-10-18 23:02:10 -0300

    Add material and light to tankobon mesh

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -10,10 +10,14 @@ export default function MyScene() {
         <Canvas className="canvas" shadows camera={{
           position: [-6, 7, 7]
         }}>
+          <ambientLight color={"green"} intensity={0.9} />
+          <pointLight position={[10, 15, 10]} />
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.Cube.geometry} />
+            geometry={nodes.Cube.geometry}
+            material={materials.Material}
+          />
         </Canvas>
       </div>
     </>
